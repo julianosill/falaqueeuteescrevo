7fc06c911ae3c6a32b237ee35ca5bbfeb3b789ae
Juliano Sill <sill.juliano@gmail.com>
2024-05-16 23:06:28 -0300

    build: enable CORS for branch test

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/api/transcribe',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value:
+              'https://falaqueeuteescuto-git-bugfix-timeout-julianosill-projects.vercel.app/',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'POST',
+          },
+        ],
+      },
+    ]
+  },
+}
 
 export default nextConfig
