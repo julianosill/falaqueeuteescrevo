fe5ee5c24871764eb4a54f69530d0905b08cadc6
Juliano Sill <sill.juliano@gmail.com>
2024-05-17 20:46:13 -0300

    fix(api): remove transcription early return

--- src/app/api/transcribe/route.ts
+++ src/app/api/transcribe/route.ts
@@ -8,11 +8,6 @@ export async function POST(request: Request) {
   const { searchParams } = new URL(request.url)
   const language = searchParams.get('lang')
 
-  return Response.json({
-    transcription:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-  })
-
   if (audioFile.type !== 'audio/wav') {
     return Response.json({ message: 'Invalid file type.' }, { status: 400 })
   }
