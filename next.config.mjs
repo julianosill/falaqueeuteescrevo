4f74b7a6049681b6e5f934b58cec25bf1f39d3f0
Juliano Sill <sill.juliano@gmail.com>
2024-05-17 11:08:39 -0300

    Revert "build: enable CORS for branch test"
    This reverts commit 7fc06c911ae3c6a32b237ee35ca5bbfeb3b789ae.

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: '/api/transcribe',
-        headers: [
-          {
-            key: 'Access-Control-Allow-Origin',
-            value:
-              'https://falaqueeuteescuto-git-bugfix-timeout-julianosill-projects.vercel.app/',
-          },
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'POST',
-          },
-        ],
-      },
-    ]
-  },
-}
+const nextConfig = {}
 
 export default nextConfig
